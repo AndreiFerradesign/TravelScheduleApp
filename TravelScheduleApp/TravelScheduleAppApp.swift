4ad192d06289a2a004df666505ba0838482db974
AndereiFerradesign <ferradesign@yandex.ru>
2024-03-13 09:24:13 +0300

    Initial Commit

---
+++ TravelScheduleApp/TravelScheduleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelScheduleAppApp.swift
+//  TravelScheduleApp
+//
+//  Created by Andrei Kashin on 13.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravelScheduleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
